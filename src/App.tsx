ce4635f6935b1aff2d1206c83822bbeed59d9537
wilsondala <dalawilson1244gmail.com>
2025-02-18 21:58:17 -0300

    atualizacao

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> 
-          Criação de <code>uma pipeline de CI/CD</code> usando o GitHub Actions.
+          Criação de <code>uma pipeline de CI/CD</code> GitHub Actions.
         </p>
         <a
           className="App-link"
