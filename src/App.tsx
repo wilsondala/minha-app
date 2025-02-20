3a4e741bc1fa67303b58ad40f6f12779a07f32a2
wilsondala <dalawilson1244gmail.com>
2025-02-20 14:13:17 -0300

    add_testeno react

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p> 
           Criação de <code>uma pipeline de CI/CD</code> GitHub Actions.
+          deploy concluido
         </p>
         <a
           className="App-link"
